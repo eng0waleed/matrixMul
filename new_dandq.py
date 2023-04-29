74747f9e5fe88fa59b5315c599fef19e6e1fc0d2
eng0waleed <eng.0waleed@gmail.com>
2023-04-29 12:23:06 +0300

    edited multiply_matrix

--- new_dandq.py
+++ new_dandq.py
@@ -31,9 +31,9 @@ def printt(display, matrix, start_row, start_column, end_row, end_column):
 # Function to add two matrices
 
 
-def add_matrix(matrix_A, matrix_B, matrix_C, split_index):
-    for i in range(split_index):
-        for j in range(split_index):
+def add_matrix(matrix_A, matrix_B, matrix_C, m):
+    for i in range(m):
+        for j in range(m):
             matrix_C[i][j] = matrix_A[i][j] + matrix_B[i][j]
 
 # Function to initialize matrix with zeros
@@ -47,50 +47,39 @@ def initWithZeros(a, r, c):
 # Function to multiply two matrices
 
 
-def multiply_matrix(matrix_A, matrix_B):
-    col_1 = len(matrix_A[0])
-    row_1 = len(matrix_A)
-    col_2 = len(matrix_B[0])
+def multiply_matrix(A, B):
+    n = len(A[0])
+    C = [[0]*n]*n
 
-    C = [[0 for x in range(col_2)] for y in range(row_1)]
-
-    if (col_1 == 1):
-        C[0][0] = matrix_A[0][0] * matrix_B[0][0]
+    if (n == 1):
+        C[0][0] = A[0][0] * B[0][0]
 
     else:
-        split_index = col_1 // 2
+        m = n // 2
 
-        row_vector = [0] * split_index
-        
-        
+        C_00 = [[0]*m]*m
+        C_01 = [[0]*m]*m
+        C_10 = [[0]*m]*m
+        C_11 = [[0]*m]*m
+        a00 = [[0]*m]*m
+        a01 = [[0]*m]*m
+        a10 = [[0]*m]*m
+        a11 = [[0]*m]*m
+        b00 = [[0]*m]*m
+        b01 = [[0]*m]*m
+        b10 = [[0]*m]*m
+        b11 = [[0]*m]*m
 
-        C_00 = [
-            [0 for x in range(split_index)] for y in range(split_index)]
-        C_01 = [
-            [0 for x in range(split_index)] for y in range(split_index)]
-        C_10 = [
-            [0 for x in range(split_index)] for y in range(split_index)]
-        C_11 = [
-            [0 for x in range(split_index)] for y in range(split_index)]
-        a00 = [[0 for x in range(split_index)] for y in range(split_index)]
-        a01 = [[0 for x in range(split_index)] for y in range(split_index)]
-        a10 = [[0 for x in range(split_index)] for y in range(split_index)]
-        a11 = [[0 for x in range(split_index)] for y in range(split_index)]
-        b00 = [[0 for x in range(split_index)] for y in range(split_index)]
-        b01 = [[0 for x in range(split_index)] for y in range(split_index)]
-        b10 = [[0 for x in range(split_index)] for y in range(split_index)]
-        b11 = [[0 for x in range(split_index)] for y in range(split_index)]
-
-        for i in range(split_index):
-            for j in range(split_index):
-                a00[i][j] = matrix_A[i][j]
-                a01[i][j] = matrix_A[i][j + split_index]
-                a10[i][j] = matrix_A[split_index + i][j]
-                a11[i][j] = matrix_A[i + split_index][j + split_index]
-                b00[i][j] = matrix_B[i][j]
-                b01[i][j] = matrix_B[i][j + split_index]
-                b10[i][j] = matrix_B[split_index + i][j]
-                b11[i][j] = matrix_B[i + split_index][j + split_index]
+        for i in range(m):
+            for j in range(m):
+                a00[i][j] = A[i][j]
+                a01[i][j] = A[i][j + m]
+                a10[i][j] = A[m + i][j]
+                a11[i][j] = A[i + m][j + m]
+                b00[i][j] = B[i][j]
+                b01[i][j] = B[i][j + m]
+                b10[i][j] = B[m + i][j]
+                b11[i][j] = B[i + m][j + m]
 
         c1 = multiply_matrix(a00, b00)
         c2 = multiply_matrix(a01, b10)
@@ -101,19 +90,19 @@ def multiply_matrix(matrix_A, matrix_B):
         c7 = multiply_matrix(a10, b01)
         c8 = multiply_matrix(a11, b11)
 
-        for i in range(split_index):
-            for j in range(split_index):
+        for i in range(m):
+            for j in range(m):
                 C_00[i][j] = c1[i][j] + c2[i][j]
                 C_01[i][j] = c3[i][j] + c4[i][j]
                 C_10[i][j] = c5[i][j] + c6[i][j]
                 C_11[i][j] = c7[i][j] + c8[i][j]
 
-        for i in range(split_index):
-            for j in range(split_index):
+        for i in range(m):
+            for j in range(m):
                 C[i][j] = C_00[i][j]
-                C[i][j + split_index] = C_01[i][j]
-                C[split_index + i][j] = C_10[i][j]
-                C[i + split_index][j + split_index] = C_11[i][j]
+                C[i][j + m] = C_01[i][j]
+                C[m + i][j] = C_10[i][j]
+                C[i + m][j + m] = C_11[i][j]
 
     return C
 
